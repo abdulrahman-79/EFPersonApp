bc0f1012171d495826541e0f2f78c6567c80acbf
Abdulrahman Haitham <abdulrahman.haitham79@gmail.com>
2025-11-04 11:49:37 +0200

    Add project files.

---
+++ EFPersonApp/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFPersonApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> EfPerson { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=" + @"D:\Luftborn\SqliteDatabases\People.db");
+        }
+    }
+}
---
+++ EFPersonApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using EFPersonApp;
+using System.Data.Common;
+
+using (var db = new AppDbContext())
+{
+    db.Database.EnsureCreated();
+
+    while(true)
+    {
+        foreach (var p in db.EfPerson)
+        {
+            Console.WriteLine($"Name: {p.Name} ----- Age: {p.Age}");
+            Console.WriteLine("\n");
+        }
+
+
+        int NewAge;
+        while (true)
+        {
+            Console.WriteLine("Enter Age: ");
+            string? ageInput = Console.ReadLine();
+
+            if (int.TryParse(ageInput, out NewAge))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+
+        Console.WriteLine("Enter Name: ");
+        string? NewName = Console.ReadLine();
+
+        db.EfPerson.Add(new Person
+        {
+            Age = NewAge,
+            Name = NewName ?? ""
+        });
+        db.SaveChanges();
+
+        Console.WriteLine("Do you want to add another entry? y/n");
+        char ans = (char)Console.Read();
+        if (ans == 'n')
+        {
+            break;
+        }
+    }
+
+}
